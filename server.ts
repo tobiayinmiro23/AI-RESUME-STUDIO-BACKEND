2e137f0e6d7dcb3c8fb06cd8eef00b5ae5ae96f8
Ayinmiro <ayinmirotobi@gmail.com>
2026-07-22 14:12:04 +0100

    improved the database url config

--- server.ts
+++ server.ts
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 // ---------- Global Middleware ----------
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_URL || "*", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL , credentials: true }));
 app.use(express.json({}));
 app.use(apiLimiter);
 
