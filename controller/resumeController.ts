588f919a850ab04d841506c7ed25551f2ebbc386
Ayinmiro <ayinmirotobi@gmail.com>
2026-07-27 14:13:04 +0100

    updated the resume route and added resume controller

--- controller/resumeController.ts
+++ controller/resumeController.ts
@@ -4,7 +4,7 @@ import { AppError } from "../utils/appError";
 
 class resumeController {
 
-    async SignUpController(req: Request, res:Response) {
+    async UploadController(req: Request, res:Response) {
             let serviceResponse = await authService.SignUp(req);
             return res.status(201).json(serviceResponse);
     }
