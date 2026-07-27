c0b3ad377c1f896955d81a71bf3a0dff2175a069
Ayinmiro <ayinmirotobi@gmail.com>
2026-07-27 13:55:05 +0100

    updated the multer middleware

--- middleware/fileUpload.ts
+++ middleware/fileUpload.ts
@@ -7,4 +7,22 @@ const storage = multer.diskStorage({
   },
 });
 
-export const upload = multer({ storage });
+export const upload = multer({ 
+    storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024, // 5 MB
+    },
+    fileFilter: (req, file, cb) => {
+    const allowedTypes = [
+      "application/pdf",
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    ];
+
+    if (!allowedTypes.includes(file.mimetype)) {
+      return cb( new Error("Only PDF and DOCX files are allowed"));
+    }
+    cb(null, true);
+
+  },
+
+});
