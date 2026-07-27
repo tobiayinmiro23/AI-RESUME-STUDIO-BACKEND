1be1696abce96e72b3619dfe5c6d249e223dabef
Ayinmiro <ayinmirotobi@gmail.com>
2026-07-27 14:59:47 +0100

    general update

--- controller/resumeController.ts
+++ controller/resumeController.ts
@@ -2,9 +2,17 @@ import { Request, Response } from "express";
 import authService from "../service/authService";
 import { AppError } from "../utils/appError";
 
+const ResumeFileValidator=(req:Request)=>{
+        if (!req.file) throw new AppError("Resume file is required", 400);
+        if (req.file.mimetype !== "application/pdf" && req.file.mimetype !== "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
+            throw new AppError("Only PDF and DOCX files are allowed", 400);
+        }
+        if (req.file.size > 5 * 1024 * 1024) throw new AppError("File cannot exceed 5MB", 400);
+    }
 class resumeController {
 
     async UploadController(req: Request, res:Response) {
+            ResumeFileValidator(req);
             let serviceResponse = await authService.SignUp(req);
             return res.status(201).json(serviceResponse);
     }
