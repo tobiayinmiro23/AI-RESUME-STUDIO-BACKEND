9e20a5538e0d4144ffdb8b8842638bba1e850e07
Ayinmiro <ayinmirotobi@gmail.com>
2026-07-28 15:33:55 +0100

    updated my open router request handler in jottings

--- jottings/openRouter.ts
+++ jottings/openRouter.ts
@@ -1,8 +1,9 @@
+import fs from "fs"
 import dotenv from "dotenv";
 dotenv.config();
 
 // fetch request example
-const getResumeDetails = async () => {
+const resumeFormat = async () => {
     const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
   method: "POST",
   headers: {
@@ -78,6 +79,74 @@ export const promptTest = async()=>{
 
 
     return data;
+  } catch (error:any) {
+    return {
+        success:false,
+        message:error.message || "an error occured"
+      }
+  }
+}
+
+export const getResumeDetail = async()=>{
+    async function encodePDFToBase64(pdfPath: string): Promise<string> {
+  const pdfBuffer = await fs.promises.readFile(pdfPath);
+  const base64PDF = pdfBuffer.toString('base64');
+  return `data:application/pdf;base64,${base64PDF}`;
+}
+
+// Read and encode the PDF
+const pdfPath = '../uploads/1785230040147-Ayinmiro Tobi B E.pdf';
+const base64PDF = await encodePDFToBase64(pdfPath);
+
+  try {
+      const response = await fetch('https://openrouter.ai/api/v1/chat/completions', {
+  method: 'POST',
+  headers: {
+    Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    model: 'google/gemma-3-27b-it',
+    messages: [
+      {
+        role: 'user',
+        content: [
+          {
+            type: 'text',
+            text: 'What are the main points in this document?',
+          },
+          {
+            type: 'file',
+            file: {
+              filename: 'document.pdf',
+              file_data: base64PDF,
+            },
+          },
+        ],
+      },
+    ],
+    // Optional: Configure PDF processing engine
+    // PDF parsing will still work even if the plugin is not explicitly set
+    plugins: [
+      {
+        id: 'file-parser',
+        pdf: {
+          engine: 'cloudflare-ai', // defaults to "mistral-ocr". See Pricing above
+        },
+      },
+    ],
+  }),
+});
+
+  if(!response.ok){
+      return {
+        success:false,
+        message:"an error occured"
+      }
+  }
+      const data = await response.json();
+    return data;
+
   } catch (error:any) {
     return {
         success:false,
