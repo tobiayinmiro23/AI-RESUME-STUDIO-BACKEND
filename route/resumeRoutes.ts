a17856d1411acd3c454b88d67bcca26396cece7f
Ayinmiro <ayinmirotobi@gmail.com>
2026-07-28 15:46:31 +0100

    updated resume controller

--- route/resumeRoutes.ts
+++ route/resumeRoutes.ts
@@ -1,14 +1,14 @@
 import express, { Request, Response } from "express";
 import resumeController from "../controller/resumeController";
 import { uploadMiddleware } from "../middleware/fileUpload";
-import { promptTest } from "../jottings/openRouter";
+import { getResumeDetail, promptTest } from "../jottings/openRouter";
 const resumeRoutes = express.Router()
 
 
 resumeRoutes.post('/upload',uploadMiddleware.single('resume'), resumeController.UploadController)
 resumeRoutes.post('/extract-data',async(req:Request,res:Response)=>{
     console.log(req.body)
-    let response = await promptTest()
+    let response = await getResumeDetail()
     console.log(response)
     res.status(200).json(response)
 })
