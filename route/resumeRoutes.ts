61a6be5e2e04f9807a01649b53177b2d364a57d8
Ayinmiro <ayinmirotobi@gmail.com>
2026-07-28 13:28:24 +0100

    setup for extracting data from the pdf

--- route/resumeRoutes.ts
+++ route/resumeRoutes.ts
@@ -1,9 +1,13 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import resumeController from "../controller/resumeController";
 import { uploadMiddleware } from "../middleware/fileUpload";
 const resumeRoutes = express.Router()
 
 
 resumeRoutes.post('/upload',uploadMiddleware.single('resume'), resumeController.UploadController)
+resumeRoutes.post('/extract-data',async(req:Request,res:Response)=>{
+    console.log(req.body)
+    res.status(200).json("i hear you")
+})
 
 export default resumeRoutes;
