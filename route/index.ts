6a7aaf24464b041f3f01f5f5f50cf52fd214a285
Ayinmiro <ayinmirotobi@gmail.com>
2026-07-30 09:42:57 +0100

    general clean up

--- route/index.ts
+++ route/index.ts
@@ -6,5 +6,4 @@ const router = express.Router()
 router.use('/api/auth', authRoutes);
 router.use('/api/resume', resumeRoutes);
 
-// app.use("/api/ats", atsRoutes);           
 export default router;
