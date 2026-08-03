c752c044823b61ec6f13a6a8bf2082ab3efd464c
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-03 16:22:45 +0100

    added get resume repository

--- repository/resumeRepository.ts
+++ repository/resumeRepository.ts
@@ -1,3 +1,4 @@
+import { getResumeDetail } from "../lib/openRouter/extractResumeDetail";
 import { ResumeModel } from "../models/resume";
 
 class ResumeRepository{
@@ -6,6 +7,9 @@ class ResumeRepository{
     }
     async createResume(userId: string, fileName: string, pdfHash: string, extractedText: string, resumeData: any) {
         return await ResumeModel.create({ userId, fileName, pdfHash, extractedText, resumeData });
+    }
+    async getResumeDetail(userId: string) {
+        return await ResumeModel.find({ userId });
     }
 }
 
