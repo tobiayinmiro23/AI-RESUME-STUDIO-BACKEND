b4702a215450840cdf934ded16f35de4aabb96ca
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-03 14:14:34 +0100

    worked on the resume route

--- service/resumeService.ts
+++ service/resumeService.ts
@@ -4,11 +4,10 @@ import { getPdfContent } from "../lib/pdfParser";
 
 class ResumeService {
     async uploadResume(req: Request) {
-        if (req.file) {
+        if (!req.file) throw new AppError("Resume file is required", 400);
             const pdfPath = `../${req.file.path}`;
             const content = await getPdfContent(pdfPath);
             return content;
-        }
         // Process the PDF content as needed
     }
 
