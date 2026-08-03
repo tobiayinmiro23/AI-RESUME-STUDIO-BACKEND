4120c3685a619b533a9c2240a36f4688b305ba03
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-03 15:00:05 +0100

    added try catch for the pdf parser

--- lib/pdfParser.ts
+++ lib/pdfParser.ts
@@ -1,13 +1,19 @@
 import { PDFParse, TextResult } from "pdf-parse";
 import fs from "fs/promises"
+import { AppError } from "../utils/appError";
 
 export async function getPdfContent(pdfPath: string): Promise<TextResult> {
-  const buffer = await fs.readFile(pdfPath);
-  const parser = new PDFParse({
-    data: buffer, 
-  });
-  const result = await parser.getText();
-  await parser.destroy();
-  return result
+  try {
+    const buffer = await fs.readFile(pdfPath);
+    const parser = new PDFParse({
+      data: buffer, 
+    });
+    const result = await parser.getText();
+    await parser.destroy();
+    return result
+  } catch (error) {
+    const message = error instanceof Error ? error.message : "Failed to extract text from PDF.";
+    throw new AppError(message, 400);
+  }
 }
   
--- service/resumeService.ts
+++ service/resumeService.ts
@@ -5,9 +5,9 @@ import { getPdfContent } from "../lib/pdfParser";
 class ResumeService {
     async uploadResume(req: Request) {
         if (!req.file) throw new AppError("Resume file is required", 400);
-            const pdfPath = `../${req.file.path}`;
-            const content = await getPdfContent(pdfPath);
-            return content;
+        const pdfPath = `../${req.file.path}`;
+        const content = await getPdfContent(pdfPath);
+        return content;
         // Process the PDF content as needed
     }
 
