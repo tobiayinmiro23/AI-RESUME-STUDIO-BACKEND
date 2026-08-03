bcb94d76b62c2d6bc1eb6c91475f28f7411b819c
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-03 15:16:19 +0100

    added resume model

---
+++ models/resume.ts
@@ -0,0 +1,49 @@
+import { Schema, model, Types } from "mongoose";
+
+const resumeSchema = new Schema(
+  {
+    userId: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+
+    fileName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    pdfHash: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+
+    resumeTitle: {
+      type: String,
+      trim: true,
+      default: null,
+    },
+
+    extractedText: {
+      type: String,
+      required: true,
+    },
+
+    resumeData: {
+      type: Schema.Types.Mixed,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+resumeSchema.index(
+  { userId: 1, pdfHash: 1 },
+  { unique: true }
+);
+export const ResumeModel = model("Resume", resumeSchema);
