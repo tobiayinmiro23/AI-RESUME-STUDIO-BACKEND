37a01b26f9a64346779780d4ba77b212e1b011aa
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-04 09:40:26 +0100

    general clean up

--- route/resumeRoutes.ts
+++ route/resumeRoutes.ts
@@ -5,11 +5,6 @@ const resumeRoutes = express.Router()
 
 
 resumeRoutes.post('/upload',uploadMiddleware.single('resume'), resumeController.UploadController)
-// resumeRoutes.post('/extract-data',async(req:Request,res:Response)=>{
-//     console.log(req.body)
-//    
-//     // console.log(response)
-//     res.status(200).json(response)
-// })
+
 
 export default resumeRoutes;
