e1fbfc5d903f8b2749d032c25dc11e096979ebc4
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-04 10:34:09 +0100

    added try catch on the resume service

--- service/resumeService.ts
+++ service/resumeService.ts
@@ -8,15 +8,19 @@ import { getResumeDetail } from "../lib/openRouter/extractResumeDetail";
 
 class ResumeService {
     async uploadResume(req: Request) {
-        if (!req.file) throw new AppError("Resume file is required", 400);
-        if (!req.userId) throw new AppError("Unauthorized request", 400);
-        const pdfPath = `../${req.file.path}`;
-        const pdfHash= await hashPdf(pdfPath);
-        const pdfHashExists = await resumeRepository.findHashByUserIdAndPdfHash(req.userId, pdfHash);
-        if(pdfHashExists)  return { message: "Resume uploaded successfully", success: true };
-        const content = await getPdfContent(pdfPath);
-        let response = await getResumeDetail(content.text)
-        await resumeRepository.createResume(req.userId, req.file.originalname, pdfHash, content.text, response);
+        try {
+            if (!req.file) throw new AppError("Resume file is required", 400);
+            if (!req.userId) throw new AppError("Unauthorized request", 400);
+            const pdfPath = `../${req.file.path}`;
+            const pdfHash= await hashPdf(pdfPath);
+            const pdfHashExists = await resumeRepository.findHashByUserIdAndPdfHash(req.userId, pdfHash);
+            if(pdfHashExists)  return { message: "Resume uploaded successfully", success: true };
+            const content = await getPdfContent(pdfPath);
+            let response = await getResumeDetail(content.text)
+            await resumeRepository.createResume(req.userId, req.file.originalname, pdfHash, content.text, response);
+        } catch (error) {
+            throw new AppError("Error uploading resume", 500);
+        }
         
     }
 
