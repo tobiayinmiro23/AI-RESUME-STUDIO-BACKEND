5896d5e0f2b7c9179318a4e796fb78889b48993f
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-04 10:42:49 +0100

    added finally to my try catch on the resume service

--- service/resumeService.ts
+++ service/resumeService.ts
@@ -4,14 +4,15 @@ import { getPdfContent } from "../lib/pdfParser";
 import {hashPdf } from "../utils/pdfHash"
 import resumeRepository from "../repository/resumeRepository";
 import { getResumeDetail } from "../lib/openRouter/extractResumeDetail";
+import { deleteUploadedFile } from "../utils/deleteFile";
 
 
 class ResumeService {
     async uploadResume(req: Request) {
+        if (!req.file) throw new AppError("Resume file is required", 400);
+        const pdfPath = `../${req.file.path}`;
         try {
-            if (!req.file) throw new AppError("Resume file is required", 400);
             if (!req.userId) throw new AppError("Unauthorized request", 400);
-            const pdfPath = `../${req.file.path}`;
             const pdfHash= await hashPdf(pdfPath);
             const pdfHashExists = await resumeRepository.findHashByUserIdAndPdfHash(req.userId, pdfHash);
             if(pdfHashExists)  return { message: "Resume uploaded successfully", success: true };
@@ -20,6 +21,8 @@ class ResumeService {
             await resumeRepository.createResume(req.userId, req.file.originalname, pdfHash, content.text, response);
         } catch (error) {
             throw new AppError("Error uploading resume", 500);
+        } finally {
+            await deleteUploadedFile(pdfPath);
         }
         
     }
