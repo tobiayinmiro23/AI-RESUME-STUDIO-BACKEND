a274bf77a70c0a0c6d1993b83ce6a3c7eb0ac4d5
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-04 10:32:10 +0100

    continued work on the resume service

--- service/resumeService.ts
+++ service/resumeService.ts
@@ -13,13 +13,10 @@ class ResumeService {
         const pdfPath = `../${req.file.path}`;
         const pdfHash= await hashPdf(pdfPath);
         const pdfHashExists = await resumeRepository.findHashByUserIdAndPdfHash(req.userId, pdfHash);
-        if(pdfHashExists){
-         
-        }else{
-            const content = await getPdfContent(pdfPath);
-            let response = await getResumeDetail(content.text)
-            await resumeRepository.createResume(req.userId, req.file.originalname, pdfHash, content.text, response);
-        }
+        if(pdfHashExists)  return { message: "Resume uploaded successfully", success: true };
+        const content = await getPdfContent(pdfPath);
+        let response = await getResumeDetail(content.text)
+        await resumeRepository.createResume(req.userId, req.file.originalname, pdfHash, content.text, response);
         
     }
 
