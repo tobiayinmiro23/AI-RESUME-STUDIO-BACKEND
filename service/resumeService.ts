c28b4327043ca3c220d12d35e0ae23bbbfc544fa
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-04 14:36:22 +0100

    general cleanup

--- service/resumeService.ts
+++ service/resumeService.ts
@@ -17,7 +17,7 @@ class ResumeService {
             const userId = userIdHeader;
             const pdfHash= await hashPdf(pdfPath);
             const pdfHashExists = await resumeRepository.findHashByUserIdAndPdfHash(userId, pdfHash);
-            console.log(pdfHashExists)
+            // console.log(pdfHashExists)
             if(pdfHashExists)  return { message: "Resume uploaded successfully", success: true };
             const content = await getPdfContent(pdfPath);
             const AIresponse = await getResumeDetail(content.text)
