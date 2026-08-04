36fbd28912fc3cfe17194436c278b20ac3401e59
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-04 14:44:22 +0100

    updated the imports for resumefile validator

--- controller/resumeController.ts
+++ controller/resumeController.ts
@@ -1,14 +1,7 @@
 import { Request, Response } from "express";
-import { AppError } from "../utils/appError";
 import resumeService from "../service/resumeService"
+import {ResumeFileValidator} from "../utils/resumeValidator"
 
-const ResumeFileValidator=(req:Request)=>{
-        if (!req.file) throw new AppError("Resume file is required", 400);
-        if (req.file.mimetype !== "application/pdf" && req.file.mimetype !== "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
-            throw new AppError("Only PDF and DOCX files are allowed", 400);
-        }
-        if (req.file.size > 5 * 1024 * 1024) throw new AppError("File cannot exceed 5MB", 400);
-    }
 class resumeController {
 
     async UploadController(req: Request, res:Response) {
