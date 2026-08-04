88671bf8ec2edb7f9b801f0415873687ae16bfcf
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-04 13:59:46 +0100

    removed error console logs

--- service/resumeService.ts
+++ service/resumeService.ts
@@ -26,7 +26,6 @@ class ResumeService {
             console.log("AI response",response)
             await resumeRepository.createResume(userId, req.file.originalname, pdfHash, content.text, response);
         } catch (error) {
-            console.log(error?.constructor.name);
             if (error instanceof AppError) throw error;
             throw new AppError("Error uploading resume", 500);
         } finally {
