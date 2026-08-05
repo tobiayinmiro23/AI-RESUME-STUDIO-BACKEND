dd92a757d2fdc48c44257a94c31a070639aa4d26
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-05 15:23:12 +0100

    general cleanup

--- lib/email/nodemailer/templates/welcome.ts
+++ lib/email/nodemailer/templates/welcome.ts
@@ -1,15 +1,10 @@
 
-
-// templates/otp.template.ts
-
 export const welcomeTemplate = (name: string,) => `
 <div style="font-family: Arial, sans-serif;">
     <p>Hello ${name},</p>
-
     <p>Welcome aboard! 🎉 Your account has been created successfully. We're excited to have you with us.</p>
-
-    <p>Best wishes Ayinmiro Tobi,</p>
-    <p>CEO & CTO @ AI Resume Studio </p>
+    <pre>Best wishes Ayinmiro Tobi,
+    CEO & CTO @ AI Resume Studio </pre>
 </div>
 `;
 
