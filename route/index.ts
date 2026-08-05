c3c776e5a4e21e45b81479c9c7b45f024a81af8a
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-05 12:39:14 +0100

    handled the emailjs response

--- route/index.ts
+++ route/index.ts
@@ -8,7 +8,7 @@ router.use('/api/v1/auth', authRoutes);
 router.use('/api/v1/resume', resumeRoutes);
 router.use('/api/v1/welcome-email', async (req:Request, res:Response)=>{
     const response= await emailService.sendEmail("tobiayinmiro1@gmail.com","tobi")
-    res.status(response.status).json(response.text)
+    res.status(response.status).json({success:true,message:"email sent successfully"})
 });
 
 export default router;
