515f5826d03e37640c143ff67a6706c944d21911
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-05 15:09:12 +0100

    general update

--- lib/email/nodemailer/sendEmail.ts
+++ lib/email/nodemailer/sendEmail.ts
@@ -24,7 +24,6 @@ class EmailService {
         html:welcomeTemplate(name),
         });
         console.log(response)
-        return response
     } catch(error) {
         if (error instanceof Error) throw new AppError(`Unable to send email: ${error.message}`, 500);
         throw new AppError("Unable to send email.", 500);
