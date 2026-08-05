87a0e15075beb50a210e9b9fe572292614e5af2e
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-05 15:27:31 +0100

    general cleanup

--- lib/email/nodemailer/templates/welcome.ts
+++ lib/email/nodemailer/templates/welcome.ts
@@ -3,8 +3,8 @@ export const welcomeTemplate = (name: string,) => `
 <div style="font-family: Arial, sans-serif;">
     <p>Hello ${name},</p>
     <p>Welcome aboard! 🎉 Your account has been created successfully. We're excited to have you with us.</p>
-    <pre>Best wishes Ayinmiro Tobi,
-    CEO & CTO @ AI Resume Studio </pre>
+    <p>Best wishes Ayinmiro Tobi,<br>
+    CEO & CTO @ AI Resume Studio </p>
 </div>
 `;
 
--- route/index.ts
+++ route/index.ts
@@ -1,19 +1,19 @@
 import express, { Request, Response } from "express";
 import authRoutes from "./authRoutes";
 import resumeRoutes from "./resumeRoutes";
-import emailService from "../lib/email/nodemailer/sendEmail";
-// import emailService from "../lib/email/emailjs/sendEmail";
+// import emailService from "../lib/email/nodemailer/sendEmail";
+import emailService from "../lib/email/emailjs/sendEmail";
 const router = express.Router()
 
 router.use('/api/v1/auth', authRoutes);
 router.use('/api/v1/resume', resumeRoutes);
 router.use('/api/v1/welcome-email', async (req:Request, res:Response)=>{
     // code for the emailjs send feature
-    // const response= await emailService.sendEmail("tobiayinmiro1@gmail.com","tobi")
-    // res.status(response.status).json({success:true,message:"email sent successfully"})
+    const response= await emailService.sendEmail("tobiayinmiro1@gmail.com","tobi")
+    res.status(response.status).json({success:true,message:"email sent successfully"})
 
-    await emailService.sendWelcomeEmail("tobiayinmiro1@gmail.com","tobi")
-    res.status(200).json({success:true,message:"email sent successfully"})
+    // await emailService.sendWelcomeEmail("tobiayinmiro1@gmail.com","tobi")
+    // res.status(200).json({success:true,message:"email sent successfully"})
 });
 
 export default router;
