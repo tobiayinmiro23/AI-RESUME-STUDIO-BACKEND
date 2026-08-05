1d6c43e5c81360ad4e6d20227b11b2c23412b4e8
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-05 15:39:01 +0100

    added helpful comments

--- lib/email/nodemailer/config.ts
+++ lib/email/nodemailer/config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false, // true if using port 465
+  secure: false, //use true in prod with port 465 in the env variable
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
