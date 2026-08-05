84799c62487c414b82d1f26bea0ff5ade53e8b41
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-05 11:59:09 +0100

    created a route to test the email feature

--- route/index.ts
+++ route/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import authRoutes from "./authRoutes";
 import resumeRoutes from "./resumeRoutes";
+import emailService from "../lib/email/sendEmail";
 const router = express.Router()
 
 router.use('/api/v1/auth', authRoutes);
 router.use('/api/v1/resume', resumeRoutes);
+router.use('/api/v1/welcome-email', ()=>{
+    emailService.sendEmail("tobicollins15@gmail.com","tobi")
+});
 
 export default router;
