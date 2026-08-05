db65c01fb47c0c8cbfafcaa07cb8f7c53e3e3bec
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-05 14:30:22 +0100

    instantiated the nodemailer class

--- lib/email/nodemailer/sendEmail.ts
+++ lib/email/nodemailer/sendEmail.ts
@@ -1,25 +1,36 @@
 // services/email.service.ts
-
 import { transporter } from "./config";
 import { AppError } from "../../../utils/appError";
+import { otpTemplate, } from "./templates/otp";
+import { welcomeTemplate, } from "./templates/welcome";
+import dotenv from "dotenv";
+dotenv.config();
 
 interface SendEmailOptions {
   to: string;
   subject: string;
   html: string;
+  name:string
   text?: string;
 }
+class EmailService {
 
-export const sendEmail = async ({to,subject,html,text}: SendEmailOptions) => {
-  try {
-    await transporter.sendMail({
-      from: process.env.SMTP_FROM,
-      to,
-      subject,
-      text,
-      html,
-    });
-  } catch {
-    throw new AppError("Unable to send email.", 500);
-  }
+ async welcomeEmail({to,name}: SendEmailOptions){
+    try {
+        await transporter.sendMail({
+        from: process.env.SMTP_FROM,
+        to,
+        subject:"AI Resume Studio",
+        // text,
+        html:welcomeTemplate(name),
+        });
+    } catch {
+        throw new AppError("Unable to send email.", 500);
+    }
 };
+
+}
+
+const emailService = new EmailService();
+
+export default emailService;
