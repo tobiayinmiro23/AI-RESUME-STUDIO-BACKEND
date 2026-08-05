f1df238071f04da11d1f8d628020b33de280cd15
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-05 11:36:37 +0100

    created a class for email feature

--- lib/email/sendEmail.ts
+++ lib/email/sendEmail.ts
@@ -1,26 +1,24 @@
 import emailjs from "@emailjs/nodejs";
 import { AppError } from "../../utils/appError";
 
-export const sendEmail = async (
-  toEmail: string,
-  subject: string,
-  message: string
-) => {
-  try {
-    await emailjs.send(
-      process.env.EMAILJS_SERVICE_ID!,
-      process.env.EMAILJS_TEMPLATE_ID!,
-      {
-        to_email: toEmail,
-        subject,
-        message,
-      },
-      {
-        publicKey: process.env.EMAILJS_PUBLIC_KEY!,
-        privateKey: process.env.EMAILJS_PRIVATE_KEY!,
-      }
-    );
-  } catch (error) {
-    throw new AppError("Unable to send email.", 500);
-  }
-};
+class EmailService {
+    async sendEmail (toEmail: string, subject: string, message: string) {
+        try {
+            await emailjs.send(
+            process.env.EMAILJS_SERVICE_ID!,
+            process.env.EMAILJS_TEMPLATE_ID!,
+            {
+                to_email: toEmail,
+                subject,
+                message,
+            },
+            {
+                publicKey: process.env.EMAILJS_PUBLIC_KEY!,
+                privateKey: process.env.EMAILJS_PRIVATE_KEY!,
+            }
+            );
+        } catch (error) {
+            throw new AppError("Unable to send email.", 500);
+        }
+    };
+}
