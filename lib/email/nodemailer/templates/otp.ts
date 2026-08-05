5ce8836eda45f845400df9789060e20e1f6c50e1
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-05 13:44:37 +0100

    added templates

---
+++ lib/email/nodemailer/templates/otp.ts
@@ -0,0 +1,18 @@
+// templates/otp.template.ts
+
+export const otpTemplate = (
+  name: string,
+  otp: string
+) => `
+<div style="font-family: Arial, sans-serif;">
+    <h2>Hello ${name},</h2>
+
+    <p>Your verification code is:</p>
+
+    <h1>${otp}</h1>
+
+    <p>This code expires in 10 minutes.</p>
+
+    <p>AI Resume Studio</p>
+</div>
+`;
