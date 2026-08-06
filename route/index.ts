a74195fea610593d0d92c32b244490e972d9bcb4
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-06 13:40:11 +0100

    general cleanup

--- route/index.ts
+++ route/index.ts
@@ -1,8 +1,6 @@
 import express, { Request, Response } from "express";
 import authRoutes from "./authRoutes";
 import resumeRoutes from "./resumeRoutes";
-// import emailService from "../lib/email/nodemailer/sendEmail";
-import emailService from "../lib/email/emailjs/sendEmail";
 const router = express.Router()
 
 router.use('/api/v1/auth', authRoutes);
