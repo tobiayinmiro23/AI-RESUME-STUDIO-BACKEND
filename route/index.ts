369e901a339c267fd7c23464df597096ea4b0dc8
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-06 09:53:48 +0100

    general update

--- route/index.ts
+++ route/index.ts
@@ -9,9 +9,10 @@ router.use('/api/v1/auth', authRoutes);
 router.use('/api/v1/resume', resumeRoutes);
 router.use('/api/v1/welcome-email', async (req:Request, res:Response)=>{
     // code for the emailjs send feature
-    await emailService.sendWelcomeEmail("tobiayinmiro1@gmail.com","tobi")
-    res.status(response.status).json({success:true,message:"email sent successfully"})
-      // code for the nodemailer send feature
+    // await emailService.sendWelcomeEmail("tobiayinmiro1@gmail.com","tobi")
+    // res.status(response.status).json({success:true,message:"email sent successfully"})
+
+    // code for the nodemailer send feature
     // await emailService.sendWelcomeEmail("tobiayinmiro1@gmail.com","tobi")
     // res.status(200).json({success:true,message:"email sent successfully"})
 });
