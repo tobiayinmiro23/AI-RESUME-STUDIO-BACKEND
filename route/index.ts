b4a217a8d92dc333388d8696bdc05211587021bf
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-06 13:40:24 +0100

    general cleanup

--- route/index.ts
+++ route/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import authRoutes from "./authRoutes";
 import resumeRoutes from "./resumeRoutes";
 const router = express.Router()
