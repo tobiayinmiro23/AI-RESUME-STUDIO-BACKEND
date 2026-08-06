53b4fb7b577002e425c40828029ff25c81f0724e
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-06 11:50:47 +0100

    updated the emailing service send otp error msg for nodemailer

--- lib/email/nodemailer/sendEmail.ts
+++ lib/email/nodemailer/sendEmail.ts
@@ -28,8 +28,8 @@ class EmailService {
             });
             console.log(response)
         } catch(error) {
-            if (error instanceof Error) throw new AppError(`Unable to send otp email: ${error.message}`, 500);
-            throw new AppError("Unable to send otp email.", 500);
+            if (error instanceof Error) throw new AppError(`Signup not successful, unable to send otp email: ${error.message} please try again`, 500);
+            throw new AppError("Signup not successful, unable to send otp email, please try again", 500);
         }
     };
 
