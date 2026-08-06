7f305f7971c3932d22801c377a2009e1ea61a37d
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-06 10:04:56 +0100

    general update

--- lib/email/nodemailer/sendEmail.ts
+++ lib/email/nodemailer/sendEmail.ts
@@ -6,20 +6,35 @@ import dotenv from "dotenv";
 dotenv.config();
 
 class EmailService {
-        async sendWelcomeEmail(to: string, name: string){
-            try {
-                await transporter.sendMail({
-                from: process.env.SMTP_FROM,
-                to,
-                subject:"AI Resume Studio",
-                // text,
-                html:welcomeTemplate(name),
-                });
-            } catch(error) {
-                if (error instanceof Error) throw new AppError(`Unable to send email: ${error.message}`, 500);
-                throw new AppError("Unable to send email.", 500);
-            }
-        };
+    async sendWelcomeEmail(to: string, name: string){
+        try {
+            await transporter.sendMail({
+            from: process.env.SMTP_FROM,
+            to,
+            subject:"AI Resume Studio",
+            // text,
+            html:welcomeTemplate(name),
+            });
+        } catch(error) {
+            if (error instanceof Error) throw new AppError(`Unable to send email: ${error.message}`, 500);
+            throw new AppError("Unable to send email.", 500);
+        }
+    };
+    async sendOtp(otp: string,to: string, name: string){
+        try {
+            await transporter.sendMail({
+            from: process.env.SMTP_FROM,
+            to,
+            subject:"AI Resume Studio",
+            // text,
+            html:welcomeTemplate(name),
+            });
+        } catch(error) {
+            if (error instanceof Error) throw new AppError(`Unable to send email: ${error.message}`, 500);
+            throw new AppError("Unable to send email.", 500);
+        }
+    };
+
 
 }
 
