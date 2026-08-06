d7fbc64f962c7f31fa7e036d8b6b15529bec1fc6
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-06 10:00:38 +0100

    general update

--- lib/email/nodemailer/templates/otp.ts
+++ lib/email/nodemailer/templates/otp.ts
@@ -5,6 +5,8 @@ export const otpTemplate = (name: string,otp: string) => `
     <p>Your verification code is:</p>
     <h1>${otp}</h1>
     <p>This code expires in 2 minutes.</p>
-    <p>AI Resume Studio</p>
+    <p>Do not share this OTP with anyone. If you didn't make this request, you can safely ignore this email.
+    AI Resume Studio will never contact you about this email or ask for any login codes or links. Beware of phishing scams.
+    Thanks for visiting AI Resume Studio!</p>
 </div>
 `;
