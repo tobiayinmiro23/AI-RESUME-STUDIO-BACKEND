0b962ac6f9d2b7d8d9418926d855a47ff2133550
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-06 09:31:48 +0100

    general cleanup

--- lib/email/nodemailer/templates/welcome.ts
+++ lib/email/nodemailer/templates/welcome.ts
@@ -1,5 +1,5 @@
 
-export const welcomeTemplate = (name: string,) => `
+export const welcomeTemplate = (name: string) => `
 <div style="font-family: Arial, sans-serif;">
     <p>Hello ${name},</p>
     <p>Welcome aboard! 🎉 Your account has been created successfully. We're excited to have you with us.</p>
