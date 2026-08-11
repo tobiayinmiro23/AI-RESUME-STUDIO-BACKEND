1bc7d1d9e36e3d1a2ce5943bd30a1def26fff6ca
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-11 13:06:59 +0100

    updated the global error handler

--- middleware/errorHandler.ts
+++ middleware/errorHandler.ts
@@ -2,9 +2,8 @@ import { NextFunction, Response,Request } from "express";
 import { AppError } from "../utils/appError";
 
 export const errorHandler = (err: AppError,_req:  Request,res: Response,next: NextFunction) => {
-   if (res.headersSent) {
-        return next(err);
-    }
+   if (res.headersSent) return next(err);
+        
   res.status(err.statusCode || 500).json({
     success: false,
     message: err.message,
