00e8f2074856e3bd5599134e3865737f8a7dc70a
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-11 12:02:19 +0100

    general cleanup

--- route/index.ts
+++ route/index.ts
@@ -7,10 +7,7 @@ router.use('/api/v1/auth', authRoutes);
 router.use('/api/v1/resume', resumeRoutes);
 // test
 router.post("/resume/status", (req, res) => {
-    res.setHeader("Content-Type", "text/event-stream");
-    res.setHeader("Cache-Control", "no-cache");
-    res.setHeader("Connection", "keep-alive");
-    res.flushHeaders();
+    
 
     res.write(`event: progress
 data: ${JSON.stringify({
