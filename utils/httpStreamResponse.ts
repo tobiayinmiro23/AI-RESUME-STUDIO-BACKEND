fec9b7070c381a8b294bdc0b2570f7193498e0d5
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-11 12:30:01 +0100

    updated the stream utils function to an async generator

--- utils/httpStreamResponse.ts
+++ utils/httpStreamResponse.ts
@@ -1,6 +1,6 @@
 import { Response } from "express";
 
-export const httpStreamResponse=(res:Response, currentAction:string)=>{
+export const asyncGeneratorResponse=(res:Response, currentAction:string)=>{
     res.write(JSON.stringify({
         stage: "parsing",
         message: currentAction
