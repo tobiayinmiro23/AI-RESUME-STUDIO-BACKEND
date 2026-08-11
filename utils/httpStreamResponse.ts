959adc963c2e85a044745e172ed3b4abd88b7c1e
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-11 12:12:32 +0100

    added a stream response util

---
+++ utils/httpStreamResponse.ts
@@ -0,0 +1,8 @@
+import { Response } from "express";
+
+export const httpStreamResponse=(res:Response)=>{
+    res.write(JSON.stringify({
+        stage: "parsing",
+        message: "Parsing resume..."
+    }) + "\n");
+}
