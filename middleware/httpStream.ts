f455130c107275a9a3bbcfb5593f3cde9235baf1
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-11 12:08:56 +0100

    updfated the stream middleware

--- middleware/httpStream.ts
+++ middleware/httpStream.ts
@@ -1,6 +1,6 @@
-import { Response,NextFunction } from "express";
+import { Response,NextFunction, Request } from "express";
 
-export const streamMiddleware = (res: Response,next: NextFunction) => {
+export const streamMiddleware = (req:Request,res: Response,next: NextFunction) => {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
