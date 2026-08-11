ebf901209f874ab1eda0778230d1f818b8c4fd5f
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-11 12:09:35 +0100

    updated the resume upload route with the stream middleware

--- route/resumeRoutes.ts
+++ route/resumeRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import resumeController from "../controller/resumeController";
 import { uploadMiddleware } from "../middleware/fileUpload";
+import { streamMiddleware } from "../middleware/httpStream";
 const resumeRoutes = express.Router()
 
 
-resumeRoutes.post('/upload',uploadMiddleware.single('resume'), resumeController.UploadController)
+resumeRoutes.post('/upload', streamMiddleware ,uploadMiddleware.single('resume'), resumeController.UploadController)
 
 
 export default resumeRoutes;
