18608e9bee50e48178633a63e2d3450f69303397
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-11 12:01:47 +0100

    updated the http stream middleware

--- middleware/httpStream.ts
+++ middleware/httpStream.ts
@@ -1,10 +1,9 @@
-import express, { Response,NextFunction } from "express";
+import { Response,NextFunction } from "express";
 
 export const streamMiddleware = (res: Response,next: NextFunction) => {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
-
   res.flushHeaders();
 
   next();
