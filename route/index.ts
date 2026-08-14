cfe08e127386f514fb6ded244cfa35114ec165dd
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-14 16:12:55 +0100

    testing the json response feature

--- route/index.ts
+++ route/index.ts
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.use('/api/v1/auth', authRoutes);
 router.use('/api/v1/resume', resumeRoutes);
-router.use("/api/v1/test",(req,res)=>{
+router.use("/api/v1/test",async (req,res)=>{
  const testSchema = {
   type: "object",
   properties: {
@@ -18,7 +18,44 @@ router.use("/api/v1/test",(req,res)=>{
   },
   required: ["name", "age"],
   additionalProperties: false
-};
+    };
+
+    const response = await fetch(
+  "https://openrouter.ai/api/v1/chat/completions",
+  {
+    method: "POST",
+
+    headers: {
+      Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify({
+      model: "openai/gpt-oss-20b:free",
+
+      messages: [
+        {
+          role: "user",
+          content: "My name is Tobi and I am 25 years old."
+        }
+      ],
+
+      response_format: {
+        type: "json_schema",
+
+        json_schema: {
+          name: "test_schema",
+          strict: true,
+          schema: testSchema
+        }
+      }
+    })
+  }
+);
+
+const data = await response.json();
+
+console.log(JSON.stringify(data, null, 2));
 })
 
 
