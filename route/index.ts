36cef24ed4a2765d697728fbfaa0b71a8ded2bde
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-14 16:11:48 +0100

    testing the json response feature

--- route/index.ts
+++ route/index.ts
@@ -5,6 +5,21 @@ const router = express.Router()
 
 router.use('/api/v1/auth', authRoutes);
 router.use('/api/v1/resume', resumeRoutes);
+router.use("/api/v1/test",(req,res)=>{
+ const testSchema = {
+  type: "object",
+  properties: {
+    name: {
+      type: "string"
+    },
+    age: {
+      type: "number"
+    }
+  },
+  required: ["name", "age"],
+  additionalProperties: false
+};
+})
 
 
 export default router;
