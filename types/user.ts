e66fea31b23d01aee3a271e2e31989d9d2e639ae
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-17 14:12:09 +0100

    updated the signin type to include refresh token

--- types/user.ts
+++ types/user.ts
@@ -3,6 +3,7 @@ export interface signInType { message: {
                 email: string,
                 id: string,
                 accessToken: string,
+                refreshToken: string,
             }, success: boolean };
 
 export interface signUpType { message: string, success: boolean };
