cc161acb4eaa0ac110f0b4d5deca124b09c7df4d
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-27 15:51:01 +0100

    updated my notes

--- aiCallInformation/models/models that actually conform to the json.md
+++ aiCallInformation/models/models that actually conform to the json.md
@@ -1,1 +1,2 @@
 "model": "nvidia/nemotron-3-super-120b-a12b:free",
+model: "liquid/lfm-2.5-2.6b:free",
--- route/index.ts
+++ route/index.ts
@@ -22,7 +22,9 @@ router.use("/api/v1/test",async (req:Request,res:Response)=> {
         },
         body: JSON.stringify({
           // model: "z-ai/glm-5.2:free",
-          model : "nvidia/nemotron-3-super-120b-a12b:free",
+          // model : "nvidia/nemotron-3-super-120b-a12b:free",
+          model: "nvidia/nemotron-3.5-lightning:free",
+          
           // model: "liquid/lfm-2.5-2.6b:free",
           // model: "openrouter/free",
           messages: [
