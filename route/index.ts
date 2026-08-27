c289cc36fe31075675b0680cb627110e4651d2e7
Ayinmiro <ayinmirotobi@gmail.com>
2026-08-27 12:59:12 +0100

    updated my notes

--- route/index.ts
+++ route/index.ts
@@ -21,7 +21,7 @@ router.use("/api/v1/test",async (req:Request,res:Response)=> {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          model: "liquid/lfm-2.5-2.6b:free",
+          model: "z-ai/glm-5.2:free",
           // model: "openrouter/free",
           messages: [
             {
